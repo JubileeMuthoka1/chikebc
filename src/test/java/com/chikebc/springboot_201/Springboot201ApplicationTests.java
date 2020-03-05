e677e8c79eddd82a88fd76d96a25200bf5d38a39
chikebc <chikecb@outlook.com>
2020-03-04 20:41:21 -0500

    Initial commit

---
+++ src/test/java/com/chikebc/springboot_201/Springboot201ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chikebc.springboot_201;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Springboot201ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
